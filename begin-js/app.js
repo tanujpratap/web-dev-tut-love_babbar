2372360609a0a8d907c695796c94b21aafda8e36
tanujpratap <tanujpratap043@gmail.com>
2024-01-16 18:02:52 +0530

    all lecture by love-babbar

---
+++ begin-js/app.js
@@ -0,0 +1,12 @@
+console.log("chaliye shuru karte h");
+// object create
+let rectangle={
+    length:1,
+    breadth:2,
+ area:function(){
+       console.log("rectangle drawing");
+    }
+};
+console.log(rectangle.length);
+console.log(rectangle.breadth);
+console.log(rectangle.area());
